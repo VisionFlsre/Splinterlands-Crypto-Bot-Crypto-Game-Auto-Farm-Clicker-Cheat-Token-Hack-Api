b75e3abfb2729f3e3bcd401fdd0a0033190cd82b
TrapzSt4r <lucaschluch@gmail.com>
2025-12-02 22:05:32 +0300

    GameState.cs

---
+++ Splinterlands-Bot-V3/WebSocket/GameState.cs
@@ -0,0 +1,15 @@
+﻿namespace SplinterlandsRObot.Models.WebSocket
+{
+    public enum WebsocketMessages
+    {
+        match_found,
+        opponent_submit_team,
+        battle_result,
+        transaction_complete,
+        rating_update,
+        ecr_update,
+        balance_update,
+        quest_progress,
+        battle_cancelled
+    };
+}
