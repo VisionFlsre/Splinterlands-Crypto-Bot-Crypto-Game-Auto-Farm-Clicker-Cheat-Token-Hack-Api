4967b3bf81b3853b27dfe8c20780131098a9af01
TrapzSt4r <lucaschluch@gmail.com>
2025-12-02 22:04:36 +0300

    SplinterlandsData.cs

---
+++ Splinterlands-Bot-V3/Global/SplinterlandsData.cs
@@ -0,0 +1,11 @@
+﻿using SplinterlandsRObot.Models;
+using SplinterlandsRObot.Models.Splinterlands;
+
+namespace SplinterlandsRObot
+{
+    public static class SplinterlandsData
+    {
+        public static List<SplinterlandsCard> splinterlandsCards = new();
+        public static SplinterlandsSettings splinterlandsSettings = new();
+    }
+}
