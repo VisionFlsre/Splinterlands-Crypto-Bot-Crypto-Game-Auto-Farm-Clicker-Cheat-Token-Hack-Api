164c237b4dcc7fb568238b7eb0048ecd88adf3f9
TrapzSt4r <lucaschluch@gmail.com>
2025-12-02 22:05:09 +0300

    FocusReward.cs

---
+++ Splinterlands-Bot-V3/Player/PlayerFocus/FocusReward.cs
@@ -0,0 +1,10 @@
+﻿namespace SplinterlandsRObot.Player.PlayerFocus
+{
+    public class FocusReward
+    {
+        public string type { get; set; }
+        public double quantity { get; set; }
+        public RewardCard card { get; set; }
+        public string potion_type { get; set; }
+    }
+}
