adc83c708ac4a06572631206076ee96b168b35f9
TrapzSt4r <lucaschluch@gmail.com>
2025-12-02 22:04:37 +0300

    Users.cs

---
+++ Splinterlands-Bot-V3/Global/Users.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml;
+using SplinterlandsRObot.Player;
+using SplinterlandsRObot.Global;
+
+namespace SplinterlandsRObot
+{
+    public class Users
+    {
+        public List<User> GetUsers()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(Path.Combine(Environment.CurrentDirectory, Constants.CONFIG_FOLDER, "users.xml"));
+            List<User> userList = new();
+            int i = 0;
+            foreach (XmlNode node in doc.DocumentElement.SelectNodes("user"))
+            {
+                userList.Add(
+                    new User
+                    {
+                        Username = Helpers.ReadNode(node, "username", true),
+                        Keys = new Keys()
+                        {
+                            ActiveKey = Helpers.ReadNode(node, "activeKEY", false, "none"),
+                            PostingKey = Helpers.ReadNode(node, "postingKEY", true),
+                            JwtToken = null,
+                            JwtExpire = DateTime.MinValue
+                        },
+                        ConfigFile = Helpers.ReadNode(node, "ConfigFile", false, "config.xml")
+                    });
+                i++;
+            }
+
+            Logs.LogMessage($"Loaded {i} users.");
+
+            return userList;
+        }
+    }
+}
