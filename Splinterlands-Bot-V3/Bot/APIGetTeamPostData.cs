6bd06f5f3dc77ce6286caf10ad0254eeeaf751fd
TrapzSt4r <lucaschluch@gmail.com>
2025-12-02 22:03:55 +0300

    APIGetTeamPostData.cs

---
+++ Splinterlands-Bot-V3/Bot/APIGetTeamPostData.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using SplinterlandsRObot.Cards;
+using SplinterlandsRObot.Player;
+
+namespace SplinterlandsRObot.Models.Bot
+{
+    public class APIGetTeamPostData
+    {
+        public JToken matchDetails { get; set; }
+        
+        public JToken questDetails { get; set; }
+        public CardsCollection playerCards { get; set; }
+        public string preferredSummoners { get; set; }
+        public string username { get; set; }
+        public int league { get; set; }
+        public bool replaceStarterCards { get; set; }
+        public bool useStarterCards { get; set; }
+        public bool prioritizeFocus { get; set; }
+        public string battleMode { get; set; }
+    }
+}
