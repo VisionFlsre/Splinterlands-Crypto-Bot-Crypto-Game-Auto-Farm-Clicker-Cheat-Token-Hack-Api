83b4e600b4711b366ebdec1cff2381bf72383d50
TrapzSt4r <lucaschluch@gmail.com>
2025-12-02 22:05:04 +0300

    UserStats.cs

---
+++ Splinterlands-Bot-V3/Player/UserStats.cs
@@ -0,0 +1,24 @@
+﻿namespace SplinterlandsRObot.Player
+{
+    public class UserStats
+    {
+        public string Account { get; set; }
+        public Balances? Balance { get; set; }
+        public double? RentCost { get; set; }
+        public int? Wins { get; set; }
+        public int? Draws { get; set; }
+        public int? Losses { get; set; }
+        public double? MatchRewards { get; set; }
+        public double? TotalRewards { get; set; }
+        public int? Rating { get; set; }
+        public string? RatingChange { get; set; }
+        public string? League { get; set; }
+        public int? CollectionPower { get; set; }
+        public string? Quest { get; set; }
+        public string? HoursUntilNextQuest { get; set; }
+        public string? Season { get; set; }
+        public DateTime NextMatchIn { get; set; }
+        public string? ErrorMessage { get; set; }
+
+    }
+}
